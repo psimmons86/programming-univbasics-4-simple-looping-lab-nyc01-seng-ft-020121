5b2a36e90fcac33d5d0b971beb714f5a062a5efd
patrick simmons <hadroncollides@gmail.com>
2021-01-15 18:27:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
